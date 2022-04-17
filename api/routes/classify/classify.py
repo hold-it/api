6806ef0ef2eb5923d2f1c8d6a7633c494d988486
Daniel Kao <78634083+Dankco@users.noreply.github.com>
2022-04-16 21:21:37 -0700

    updated model

--- api/routes/classify/classify.py
+++ api/routes/classify/classify.py
@@ -1,9 +1,16 @@
 from model.model import classifyInput
 from newspaper import Article
+from flask import Blueprint
+_classify = Blueprint('classify', __name__)
 
-
-url = "https://en.wikipedia.org/wiki/Jimmy_Neutron"
-article = Article(url)
-article.download()
-article.parse()
-classifyInput(article.text)
+@_classify.route('/url', methods=['POST'])
+def extract():
+    request_url = request.get_json()
+    url = None
+    if request_url:
+        if 'url' in request_url:
+            url = request_url['url']
+    article = Article(url)
+    article.download()
+    article.parse()
+    return classifyInput(article.text);
--- api/routes/classify/model/model.py
+++ api/routes/classify/model/model.py
@@ -1,9 +1,9 @@
-
 import pandas as pd
 import os, re, string, nltk, pickle
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
 from keras.preprocessing import text,sequence
+import tensorflow as tf
 max_features = 10000
 maxlen = 300
 
@@ -14,36 +14,24 @@ def classifyInput(texts):
     punctuation = list(string.punctuation)
     stop.update(punctuation)
     testing_data = pd.DataFrame(data = d)
-    def strip_html(text):
-        soup = BeautifulSoup(text, "html.parser")
-        return soup.get_text()
+    testing_data = pd.DataFrame(data = d)
 
-    #Removing the square brackets
-    def remove_between_square_brackets(text):
-        return re.sub('\[[^]]*\]', '', text)
-    # Removing URL's
-    def remove_between_square_brackets(text):
-        return re.sub(r'http\S+', '', text)
-    #Removing the stopwords from text
-    def remove_stopwords(text):
+    def clean(text):
+        soup = BeautifulSoup(text, "html.parser")
+        text = soup.get_text()
+        text = re.sub('\[[^]]*\]', '', text)
+        text = re.sub(r'http\S+', '', text)
         final_text = []
         for i in text.split():
             if i.strip().lower() not in stop:
                 final_text.append(i.strip())
         return " ".join(final_text)
-    #Removing the noisy text
-    def denoise_text(text):
-        text = strip_html(text)
-        text = remove_between_square_brackets(text)
-        text = remove_stopwords(text)
-        return text
     #Apply function on review column
-    testing_data['text']=testing_data['text'].apply(denoise_text)
-    #transform the data
-    tokenizer = text.Tokenizer(num_words=max_features)
+    testing_data['text']=testing_data['text'].apply(clean)
+    tokenizer = text.Tokenizer(num_words = max_features)
     tokenized_test = tokenizer.texts_to_sequences(testing_data['text'])
     test_model = sequence.pad_sequences(tokenized_test, maxlen=maxlen)
-    # load the model and predict
-    loaded_model = pickle.load(open(os.path.join(os.getcwd(), 'api/routes/classify/model/Best_Model.pkl'),'rb'))
+    # load the model and predict'
+    loaded_model = pickle.load(open(os.path.join(os.getcwd(),"./api/api/routes/classif/model/Best_model.pkl"),'rb'))
     result = loaded_model.predict(test_model)
     return result
